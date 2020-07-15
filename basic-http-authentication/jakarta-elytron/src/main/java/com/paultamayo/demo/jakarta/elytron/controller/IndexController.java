45d5f1194d09db1a3d71375bae9b341ce61d691e
Paul Tamayo <dpaul.tamayo@yahoo.com>
2020-07-15 17:27:50 -0500

    + Agregando proyecto de autenticación jakarta y elytron.

---
+++ basic-http-authentication/jakarta-elytron/src/main/java/com/paultamayo/demo/jakarta/elytron/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.paultamayo.demo.jakarta.elytron.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+
+	private static final long serialVersionUID = 5628857443808500882L;
+
+}
