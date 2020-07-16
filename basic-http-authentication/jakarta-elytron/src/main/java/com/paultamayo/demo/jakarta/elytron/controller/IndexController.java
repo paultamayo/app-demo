a9c037631d789dbc5b42c0392fdfff699854d074
Paul Tamayo <dpaul.tamayo@yahoo.com>
2020-07-16 14:52:32 -0500

    + Agregando principal al demo

--- basic-http-authentication/jakarta-elytron/src/main/java/com/paultamayo/demo/jakarta/elytron/controller/IndexController.java
+++ basic-http-authentication/jakarta-elytron/src/main/java/com/paultamayo/demo/jakarta/elytron/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.paultamayo.demo.jakarta.elytron.controller;
 
 import java.io.Serializable;
+import java.security.Principal;
 
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -10,5 +12,17 @@ import javax.inject.Named;
 public class IndexController implements Serializable {
 
 	private static final long serialVersionUID = 5628857443808500882L;
+	
+	private Principal principal;
+	
+	
+	
+	public String getHello() {
+		principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+		
+		System.out.println(principal);
+		
+		return principal.getName();
+	}
 
 }
